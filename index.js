fd0e8028b21c44e0becac4a1a8005d37e9dcb313
Gabriel <relkovic10@gmail.com>
2021-06-18 01:16:30 +0200

    final version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const $ = document.querySelectorAll.bind(document);
-
 const sounds = [
     {id:'Bass Drum'},
     {id:'Snare'},
@@ -10,8 +9,9 @@ const sounds = [
     {id:'Cowbell'},
     {id:'Rack Tom'},
     {id:'Floor Tom'}
-  ];
-
+];
+const audioElements = $('audio');
+const display = $('.name')[0];
 let volumeVal = 0.5;
 
 $('input[type=range]')[0].addEventListener('input', function(e) {
@@ -19,101 +19,66 @@ $('input[type=range]')[0].addEventListener('input', function(e) {
     volumeVal = e.target.value / 100;
 });
 
-window.onkeydown = function(e) {
-    function seter(id) {
+function seter(id) {
+    setTimeout(() => {
+        $(`.${id}-but`)[0].classList.add('pressed');
         setTimeout(() => {
-            $(`.${id}-but`)[0].classList.add('pressed');
-            setTimeout(() => {
-                $(`.${id}-but`)[0].classList.remove('pressed');
-            }, 140);
-        }, 0);
-    }
-    switch(e.key) {
-        case 'q':
-            const audioQ = $("#bass")[0];
-            audioQ.currentTime = 0;
-            audioQ.volume = parseFloat(volumeVal);
-            audioQ.play();
-            $('.name')[0].textContent = sounds[0].id;
-            seter('q');
-        break;
-        case 'w':
-            const audioW = $("#snare")[0];
-            audioW.currentTime = 0;
-            audioW.volume = parseFloat(volumeVal);
-            audioW.play();
-            $('.name')[0].textContent = sounds[1].id;
-            seter('w');
-        break;
-        case 'e':
-            const audioE = $("#closed")[0];
-            audioE.currentTime = 0;
-            audioE.volume = parseFloat(volumeVal);
-            audioE.play();
-            $('.name')[0].textContent = sounds[2].id;
-            seter('e');
-        break;
-        case 'a':
-            const audioA = $("#opened")[0];
-            audioA.currentTime = 0;
-            audioA.volume = parseFloat(volumeVal);
-            audioA.play();
-            $('.name')[0].textContent = sounds[3].id;
-            seter('a');
-        break;
-        case 's':
-            const audioS = $("#crash")[0];
-            audioS.currentTime = 0;
-            audioS.volume = parseFloat(volumeVal);
-            audioS.play();
-            $('.name')[0].textContent = sounds[4].id;
-            seter('s');
-        break;
-        case 'd':
-            const audioD = $("#ride")[0];
-            audioD.currentTime = 0;
-            audioD.volume = parseFloat(volumeVal);
-            audioD.play();
-            $('.name')[0].textContent = sounds[5].id;
-            seter('d');
-        break;
-        case 'y':
-            const audioY = $("#cow")[0];
-            audioY.currentTime = 0;
-            audioY.volume = parseFloat(volumeVal);
-            audioY.play();
-            $('.name')[0].textContent = sounds[6].id;
-            seter('y');
-        break;
-        case 'x':
-            const audioX = $("#rack")[0];
-            audioX.currentTime = 0;
-            audioX.volume = parseFloat(volumeVal);
-            audioX.play();
-            $('.name')[0].textContent = sounds[7].id;
-            seter('x');
-        break;
-        case 'c':
-            const audioC = $("#floor")[0];
-            audioC.currentTime = 0;
-            audioC.volume = parseFloat(volumeVal);
-            audioC.play();
-            $('.name')[0].textContent = sounds[8].id;
-            seter('c');
-        break;
-    }
+            $(`.${id}-but`)[0].classList.remove('pressed');
+        }, 140);
+    }, 0);
 }
 
-const audioElements = $('audio');
-const display = $('.name')[0];
+function main(audTag, name) {
+    let audio = audTag;
+    audio.currentTime = 0;
+    audio.volume = parseFloat(volumeVal);
+    audio.play();
+    display.textContent = name;
+}
 
 $('button').forEach((but, i) => {
     but.addEventListener('click', () => {
-        let audio = audioElements[i];
-        audio.currentTime = 0;
-        audio.volume = parseFloat(volumeVal);
-        audio.play();
-        display.textContent = sounds[i].id;
+        main(audioElements[i], sounds[i].id);
     });
 });
 
+window.onkeydown = function(e) {
+    switch(e.key) {
+        case 'q':
+            main(audioElements[0], sounds[0].id);
+            seter(e.key);
+        break;
+        case 'w':
+            main(audioElements[1], sounds[1].id);
+            seter(e.key);
+        break;
+        case 'e':
+            main(audioElements[2], sounds[2].id);
+            seter(e.key);
+        break;
+        case 'a':
+            main(audioElements[3], sounds[3].id);
+            seter(e.key);
+        break;
+        case 's':
+            main(audioElements[4], sounds[4].id);
+            seter(e.key);
+        break;
+        case 'd':
+            main(audioElements[5], sounds[5].id);
+            seter(e.key);
+        break;
+        case 'y':
+            main(audioElements[6], sounds[6].id);
+            seter(e.key);
+        break;
+        case 'x':
+            main(audioElements[7], sounds[7].id);
+            seter(e.key);
+        break;
+        case 'c':
+            main(audioElements[8], sounds[8].id);
+            seter(e.key);
+        break;
+    }
+}
